18b12ade10343ab9b8e5fed8ed78a8857f625797
cyberjen <jenlawone@gmail.com>
2023-11-20 14:34:34 -0500

    second commit

--- loader.py
+++ loader.py
@@ -2,15 +2,22 @@ import requests
 import boto3
 import sqlite3
 import config
+import json 
 connection = sqlite3.connect(config.DB_PATH)
 links = connection.execute("select id, Link,PropertyProcessNo, DocumentType from Documents").fetchAll()
 client = boto3.client("s3")
+errors = []
 for uid, link, process,documenttype in links:
-    response = requests.get(link)
-    client.put_object(
-        Body= response.content, 
-        Bucket= "condorepository",
-        Key = f"{process}-{documenttype}-{uid}.pdf"
-
-    )
-    print("Document is Loaded", process)
+    try: 
+        response = requests.get(link)
+        client.put_object(
+            Body= response.content, 
+            Bucket= "condorepository",
+            Key = f"{process}-{documenttype}-{uid}.pdf"
+        )
+        print("Document is Loaded", process)
+    except Exception as e:
+        errors.append(uid)
+        print(e) 
+with open("errors.txt", "w") as file : 
+    json.dump(errors, file)
